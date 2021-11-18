8245818a23b46b325359d684f2dfd917bf0c6f97
LuchoEmilio <lechica@uninorte.edu.co>
2021-11-18 16:10:31 -0500

    vuelvo hacer cambios en master

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
 console.log("PRUEBA LUIS EMILIO");
 console.log("segunda linea de codigo");
 console.log("tercera linea");
+console.log("comentario desde dev1 ahroa");
+console.log("linea desde master nuevamente");
